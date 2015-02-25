5444c01c7783e74c638812d530e205f868653a79
Rodrigo Morais <morais.rm@gmail.com>
2015-02-25 17:38:05 -0300

    Adjust the blueprint.

--- blueprints/ember-json-pretty/index.js
+++ blueprints/ember-json-pretty/index.js
@@ -1,11 +1,9 @@
 module.exports = {
+  description: ''
+     
   normalizeEntityName: function() {},
 
-  afterInstall: function() {
-    var that = this;
-
-    return this.addBowerPackageToProject('ember-json-pretty').then(function() {
-        return that.addBowerPackageToProject('components-font-awesome');
-    });
+  afterInstall: function(options) {
+    return this.addBowerPackageToProject('components-font-awesome');
   }
 };
