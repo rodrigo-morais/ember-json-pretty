79599e57ec722797b15f5b12cd3d67596f552700
Rodrigo Morais <morais.rm@gmail.com>
2015-02-25 16:51:43 -0300

    Adjusting project to publish.

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
     app.import('vendor/ember-json-pretty/styles/styles.css');
-    app.import('bower_components/components-font-awesome/css/font-awesome.min.css');
-    app.import('bower_components/components-font-awesome/fonts/FontAwesome.otf', { destDir: 'fonts' });
-    app.import('bower_components/components-font-awesome/fonts/fontawesome-webfont.eot', { destDir: 'fonts' });
-    app.import('bower_components/components-font-awesome/fonts/fontawesome-webfont.svg', { destDir: 'fonts' });
-    app.import('bower_components/components-font-awesome/fonts/fontawesome-webfont.ttf', { destDir: 'fonts' });
-    app.import('bower_components/components-font-awesome/fonts/fontawesome-webfont.woff', { destDir: 'fonts' });
+    app.import(app.bowerDirectory + '/components-font-awesome/css/font-awesome.min.css');
+    app.import(app.bowerDirectory + '/components-font-awesome/fonts/FontAwesome.otf', { destDir: 'fonts' });
+    app.import(app.bowerDirectory + '/components-font-awesome/fonts/fontawesome-webfont.eot', { destDir: 'fonts' });
+    app.import(app.bowerDirectory + '/components-font-awesome/fonts/fontawesome-webfont.svg', { destDir: 'fonts' });
+    app.import(app.bowerDirectory + '/components-font-awesome/fonts/fontawesome-webfont.ttf', { destDir: 'fonts' });
+    app.import(app.bowerDirectory + '/components-font-awesome/fonts/fontawesome-webfont.woff', { destDir: 'fonts' });
   }
 };
