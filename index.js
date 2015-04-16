93fd012bbe3f33ff867214f8e43edf5d9a5f70a4
Taras Mankovski <tarasm@gmail.com>
2015-04-16 11:23:04 -0400

    Replaced Font Awesome with ember-cli-font-awesome

--- index.js
+++ index.js
@@ -6,11 +6,5 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
     app.import('vendor/ember-json-pretty/styles/styles.css');
-    app.import(app.bowerDirectory + '/components-font-awesome/css/font-awesome.min.css');
-    app.import(app.bowerDirectory + '/components-font-awesome/fonts/FontAwesome.otf', { destDir: 'fonts' });
-    app.import(app.bowerDirectory + '/components-font-awesome/fonts/fontawesome-webfont.eot', { destDir: 'fonts' });
-    app.import(app.bowerDirectory + '/components-font-awesome/fonts/fontawesome-webfont.svg', { destDir: 'fonts' });
-    app.import(app.bowerDirectory + '/components-font-awesome/fonts/fontawesome-webfont.ttf', { destDir: 'fonts' });
-    app.import(app.bowerDirectory + '/components-font-awesome/fonts/fontawesome-webfont.woff', { destDir: 'fonts' });
   }
 };
